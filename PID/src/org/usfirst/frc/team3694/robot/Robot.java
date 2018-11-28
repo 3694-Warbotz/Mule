74517bedc37d55b8110d4e46a7a96c5b680fe4e4
mcapoor <40697079+mcapoor@users.noreply.github.com>
2018-11-28 18:38:34 -0500

    Normalised PI control
    Normalised the PI output. Instead of passing values 180+ to the victor, this should now keep the output between -1.0 and +1.0.

--- PID/src/org/usfirst/frc/team3694/robot/Robot.java
+++ PID/src/org/usfirst/frc/team3694/robot/Robot.java
@@ -2,14 +2,14 @@
 
 package org.usfirst.frc.team3694.robot;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
+import com.analog.adis16448.frc.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends IterativeRobot {
+	public static final ADIS16448_IMU imu = new ADIS16448_IMU();
 	
 	Victor left = new Victor(0);
 		//victor on robot port 0
@@ -19,10 +19,12 @@ public class Robot extends IterativeRobot {
 		//joystick on DS port 1
 	Joystick rightStick = new Joystick(2);
 		//joystick on DS port 2
+	DifferentialDrive robotDrive = new DifferentialDrive(left, right);
 	
-	double integral = 1;
-	double proportional = 1;
-	double out = proportional + integral;
+	double integral = 1; 
+		//kp times sum error
+	double proportional = 1; 
+		//kp times error
 
 	@Override
 	public void robotInit() {
@@ -32,44 +34,93 @@ public class Robot extends IterativeRobot {
 	public void autonomousInit() {
 	}
 
-	public double PI(double setpoint, double kp, double ki) {
+	public double[] PI(double setpoint, double kp, double ki, double offset) {
 		//setpoint is the degrees you want to turn 
 		//kp is the proportional constant found by trial
 		//ki is the integral constant found by trial
+		//offset is the margin of error
 		
-		Gyro gyro = new AnalogGyro(1);
+		double cumulativeError = 0;
+			//initial error values before calculation
+		double count = 1;
+			//'count' is the number of times the output is calculated
+		double out = 0;
 		
-		//initial error values before calculation
-		double previousError = 0;
-		
-		double angle = gyro.getAngle();
-		double error = setpoint - angle;
+		double angle = imu.getAngleZ();
+		double currentError = setpoint - angle;
 			//error = target - actual
-		
-		while (angle < setpoint || angle > setpoint) {
+
+		if (Math.abs(setpoint - angle) > offset) {
 			//while angle is not correct
-				angle -= gyro.getAngle();
+				angle = imu.getAngleZ();
 					//update angle
-				error -= setpoint - angle; 
+				currentError = setpoint - angle; 
+				cumulativeError += currentError;
 					//update error
-				integral += previousError * (error * ki);
+				integral = cumulativeError * ki;
 					//calculate integral
 					//sigma error * integral constant
-				proportional += error * kp;
+				proportional = currentError * kp;
 					//calculate proportion
 					//how far we are off * how much we are changing
-				previousError += error; 
-					//add the current error to the sum of all past error 	
+					//add the current error to the sum of all past error 
+					
+				out =  proportional + integral;
+					
+				count++; 
+					//updates iteration count for use with mean
+		}	
+		
+		return new double[] {out, count};
+			//return the PI output as [0] and the number of iterations as [1] and the previous output is [2]		
+	}
+	
+	public double normalise(double meanOut, double maxOut, double minOut) {
+		double results[] = PI(90, 0.25, 0.1, 1.0);
+			//gets the output of the PI function to be normalised
+		
+		double currentOut = results[0];
+		double count = results[1];
+		
+		
+		if (isAutonomous()) {
+			double cumulativeOut = 0;
+			
+			if (count == 1) {
+				maxOut = currentOut; 
+				minOut = currentOut; 
+				//when starting there is only one output 
+			} else {
+				if (Math.max(currentOut, maxOut) != maxOut) {
+					maxOut = currentOut; 
+				//if the new output is higher than the previous max, update the max
+				}
+					
+				if (Math.min(currentOut, minOut) != minOut) {
+					minOut = currentOut; 
+				//if the new output is lower than the previous min, update the min
+				}
+			}
+			
+			cumulativeOut += currentOut; 
+				//sum all the outputs
+			meanOut = cumulativeOut / count;
+				//find the mean of the outputs
 		}
-		return out;
+		
+		return (currentOut - meanOut) / (maxOut - minOut);
+			//normalisation formula
 	}
 	
 	@Override
 	public void autonomousPeriodic() {
-		PI(90, 0.25, 0.1);
-		left.set(out);
-		right.set(out);
-		//set drivetrain to the output
+		double norm = normalise(0, 0, 0);
+		
+		robotDrive.arcadeDrive(0, norm);
+			//set drivetrain to the output
+		
+		System.out.println("Out: " + norm);
+		System.out.println("Angle: " + imu.getAngleZ());
 	}
 
 	public void teleopPeriodic() {
