6e58400cf7f8ab31e70883fd0bcaf8c5f24e2640
MitchellConrad <51652487+MitchellConrad@users.noreply.github.com>
2019-10-29 17:28:34 -0400

    okay

--- encoders/src/main/java/frc/robot/Robot.java
+++ encoders/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,10 @@
 
 package frc.robot;
 
+import java.lang.reflect.Array;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Victor;
@@ -32,7 +36,30 @@ public class Robot extends IterativeRobot {
   SpeedControllerGroup left = new SpeedControllerGroup(leftFront, leftBack);
   SpeedControllerGroup right = new SpeedControllerGroup(rightFront, rightBack);
 
-  Encoder enc = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+  Encoder rightFrontEnc = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+  Encoder leftFrontEnc = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
+  Encoder rightBackEnc = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+  Encoder leftBackEnc = new Encoder(6, 7, false, Encoder.EncodingType.k4X);
+
+  PIDController pidLeftFront = new PIDController(0,0,0, leftFrontEnc, leftFront);
+  leftFrontEnc.setPIDSourceType(PIDSourceType.kDisplacement); 
+
+  
+
+  
+
+  double[] distances = {540, 1956, 1572, 120, 192, 780, 528, 372}; 
+  double[] turns = {-90, 180, 90, 90, -90, -90, -90}; 
+
+  double startPoint;
+
+  //diameter of wheels = 6 inches
+  //circumference of wheels = 18.84 inches
+
+
+
+
+
 
 
 
@@ -62,6 +89,16 @@ public class Robot extends IterativeRobot {
   
   @Override
   public void autonomousPeriodic() {
+    for (int i = 0; i < 6; i++) {
+      double startPoint = 0;
+      double distance = distances[i];
+      double turn = turns[i];
+
+      
+  
+
+    }
+  
   }
  
 
