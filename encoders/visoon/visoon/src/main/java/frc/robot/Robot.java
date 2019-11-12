af613516c82286a7921414e817da6c98e9937e1c
MitchellConrad <51652487+MitchellConrad@users.noreply.github.com>
2019-11-12 17:29:16 -0500

    last save pls work

---
+++ encoders/visoon/visoon/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class Robot extends IterativeRobot {
+
+  @Override
+  public void robotInit() {
+    
+  }
+
+  
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  
+  }
+
+  public void autonomousPeriodic() {
+    
+  }
+  
+
+  
+  public void teleopPeriodic() {
+  }
+
+  
+  @Override
+  public void testPeriodic() {
+  }
+}
