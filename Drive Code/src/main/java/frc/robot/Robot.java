5e23c4484a16970de2699cdfd625b3964bf33c46
Milan Capoor <capoorm123@gmail.com>
2020-01-25 22:11:30 -0500

    tank drive

---
+++ Drive Code/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,54 @@
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Victor;
+
+public class Robot extends TimedRobot {
+	public static Victor leftFront = new Victor(0);
+	public static Victor rightFront = new Victor(1);
+	public static Victor leftBack = new Victor(2);
+	public static Victor rightBack = new Victor(3);
+	public static SpeedControllerGroup left = new SpeedControllerGroup(leftFront, leftBack);
+	public static SpeedControllerGroup right = new SpeedControllerGroup(rightFront, rightBack);
+
+	public static Joystick leftStick = new Joystick(0);
+	public static Joystick rightStick = new Joystick(1);
+
+	@Override
+	public void robotInit() {
+		CameraServer.getInstance().startAutomaticCapture();
+	}
+
+
+	@Override
+	public void robotPeriodic() {
+	}
+
+
+	@Override
+	public void autonomousInit() {
+	
+	}
+
+
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	
+	@Override
+	public void teleopPeriodic() {
+		left.set(-leftStick.getY());
+		right.set(rightStick.getY());
+	}
+
+
+	@Override
+	public void testPeriodic() {
+	}
+}
